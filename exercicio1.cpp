87d628e7a9444fd34dffa81665c6648399a835da
AlanCornely <162385514+AlanCornely@users.noreply.github.com>
2024-04-15 23:11:40 +0000

    exemplos

---
+++ exercicio1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+
+    string str;
+    
+    cout <<"digite uma string:"<< endl;
+    cin >> str;
+    
+    cout << "A string digitada:" << str << endl;
+    
+    return 0;
+}
