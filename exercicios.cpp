2906a560b3bccc04f11626e78cc594fb9f3c62d5
AlanCornely <162385514+AlanCornely@users.noreply.github.com>
2024-04-16 00:50:00 +0000

    em andamento

---
+++ exercicios.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main (){
+    
+    int arr[5];
+    printf("digite 5 numeros:");
+    scanf("%d, %d, %d, %d, %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5]);
+
+    int arrContrario = arr[5], arr[4], arr[3], arr[2], arr[1], arr[0];
+    printf (" os números digitados na forma contrária são: %d\n", arrContrario);
+
+
+//--------------------------------------------------------------------------------------------------
+
+    int arr[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
